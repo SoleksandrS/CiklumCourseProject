6df20e2b071a9aafcd58edc1dfd5c87e05be1f29
SoleksandrS <o.y.sitaylo@student.khai.edu>
2023-11-03 23:32:04 +0200

    feat: add Button and add some styles

--- X-course-task/src/pages/NotFound/NotFound.tsx
+++ X-course-task/src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../../components';
+
+import styles from './NotFound.module.scss';
+
 function NotFound() {
+  const navigate = useNavigate();
+
   return (
-    <div>
+    <div className={styles['not-found']}>
       <h1>Not found</h1>
+      <Button typeStyleBtn="primary" onClick={() => navigate('/books')}>
+        Go to home
+      </Button>
     </div>
   );
 }

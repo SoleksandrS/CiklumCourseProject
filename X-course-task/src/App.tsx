09af652f0bc9eb5b98107d2e9965b2b0f870d598
SoleksandrS <o.y.sitaylo@student.khai.edu>
2023-11-03 16:55:34 +0200

    fix: adjust initial styles

--- X-course-task/src/App.tsx
+++ X-course-task/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Routing from './routing/Routing';
-import './App.scss';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(

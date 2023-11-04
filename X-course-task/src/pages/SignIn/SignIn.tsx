62c47da4eaa215f1a73c4f78a0ec36eb4656e8d4
SoleksandrS <o.y.sitaylo@student.khai.edu>
2023-11-04 12:13:09 +0200

    feat: add sign-in form

--- X-course-task/src/pages/SignIn/SignIn.tsx
+++ X-course-task/src/pages/SignIn/SignIn.tsx
@@ -1,7 +1,28 @@
+import { useCallback } from 'react';
+import { Button } from '../../components';
+
+import styles from './SignIn.module.scss';
+
 function SignIn() {
+  const onSubmitHandler = useCallback((event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(event);
+  }, []);
+
   return (
-    <div>
+    <div className={styles['sign-in']}>
       <h1>Sign in</h1>
+      <form className={styles['form']} onSubmit={onSubmitHandler}>
+        <div className={styles['row']}>
+          <label className={styles['label']} htmlFor="user-name">
+            Username
+          </label>
+          <input className={styles['input']} type="text" id="user-name" name="user-name" />
+        </div>
+        <Button type="submit" typeStyleBtn="primary">
+          Sign-In
+        </Button>
+      </form>
     </div>
   );
 }

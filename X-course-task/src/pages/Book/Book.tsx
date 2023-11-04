05d552ef53e0544345002a759b4e9a228d83ed87
SoleksandrS <o.y.sitaylo@student.khai.edu>
2023-11-04 19:20:33 +0200

    feat: add content of Book page

--- X-course-task/src/pages/Book/Book.tsx
+++ X-course-task/src/pages/Book/Book.tsx
@@ -1,12 +1,65 @@
+import { useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import books from '../../assets/data/books.json';
+import { NotFound } from '../';
+
+import styles from './Book.module.scss';
+import { Button } from '../../components';
 
 function Book() {
   const { id } = useParams();
 
+  const [count, setCount] = useState(1);
+
+  const book = useMemo(() => books.find((obj) => obj.id === +id!), [id]);
+
   return (
-    <div>
-      <h1>Book {id}</h1>
-    </div>
+    <>
+      {book ? (
+        <div className={styles['book']}>
+          <div className={styles['info-block']}>
+            <div className={styles['row']}>
+              <img src={book.image} alt={book.title} />
+              <div className={styles['column']}>
+                <h1 className={styles['title']}>{book.title}</h1>
+                <p className={styles['paragraph']}>Author(s): {book.author}</p>
+                <p className={styles['paragraph']}>Book level: {book.level}</p>
+                <p className={styles['paragraph']}>Book tags: {book.tags.join(', ')}</p>
+              </div>
+            </div>
+            <p className={styles['description']}>{book.description}</p>
+          </div>
+          <div className={styles['price-block']}>
+            <div className={styles['row']}>
+              <p>Price, $</p>
+              <span>{book.price}</span>
+            </div>
+            <div className={styles['row']}>
+              <p>Count</p>
+              <input
+                type="number"
+                min="1"
+                max={book.amount}
+                value={count}
+                onChange={(event) => setCount(+event.target.value)}
+              />
+            </div>
+            <div className={styles['row']}>
+              <p>Total price, $</p>
+              <span>{(book.price * count).toFixed(2)}</span>
+            </div>
+            <Button
+              onClick={() => console.log(count)}
+              className={styles['btn']}
+              typeStyleBtn="primary">
+              Add to cart
+            </Button>
+          </div>
+        </div>
+      ) : (
+        <NotFound />
+      )}
+    </>
   );
 }
 

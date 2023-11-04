a92b5f8eda74dd707136cbd80cf7e0536e5f2413
SoleksandrS <o.y.sitaylo@student.khai.edu>
2023-11-04 13:08:55 +0200

    feat: add styles for books list

--- X-course-task/src/pages/Books/Books.tsx
+++ X-course-task/src/pages/Books/Books.tsx
@@ -1,12 +1,16 @@
 import books from '../../assets/data/books.json';
 
+import styles from './Books.module.scss';
+
 function Books() {
   return (
-    <div>
+    <div className={styles['books']}>
       <h1>Books</h1>
-      <ul>
+      <ul className={styles['list']}>
         {books.map((book) => (
-          <li key={`book-${book.id}`}>{book.title}</li>
+          <li key={`book-${book.id}`} className={styles['book-item']}>
+            {book.title}
+          </li>
         ))}
       </ul>
     </div>

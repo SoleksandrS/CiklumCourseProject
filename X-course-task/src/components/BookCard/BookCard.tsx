5c9fcbf8814b739b472e22a61cf5d35eedcf1304
SoleksandrS <o.y.sitaylo@student.khai.edu>
2023-11-04 14:03:48 +0200

    feat: add links to specific book in BookCard component

--- X-course-task/src/components/BookCard/BookCard.tsx
+++ X-course-task/src/components/BookCard/BookCard.tsx
@@ -1,3 +1,4 @@
+import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '../';
 import BookType from '../../types/BookType';
 
@@ -11,10 +12,11 @@ interface BookCardProps {
 
 function BookCard(props: BookCardProps) {
   const { book } = props;
+  const navigate = useNavigate();
 
   return (
     <div className={styles['book-card']}>
-      <div className={styles['image-block']}>
+      <Link to={`/books/${book.id}`} className={styles['image-block']}>
         <img
           className={styles['image']}
           src={book.image}
@@ -24,13 +26,15 @@ function BookCard(props: BookCardProps) {
             image.src = notFoundIcon;
           }}
         />
-      </div>
+      </Link>
       <div className={styles['info-block']}>
         <h5 className={styles['title']}>{book.title}</h5>
         <p className={styles['author']}>{book.author}</p>
         <div className={styles['row']}>
           <p className={styles['price']}>${book.price}</p>
-          <Button typeStyleBtn="transparent">View</Button>
+          <Button onClick={() => navigate(`/books/${book.id}`)} typeStyleBtn="transparent">
+            View
+          </Button>
         </div>
       </div>
     </div>

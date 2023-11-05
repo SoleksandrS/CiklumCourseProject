ed5121028f578d32a6ad6ae1cec1f57a363e2be5
SoleksandrS <o.y.sitaylo@student.khai.edu>
2023-11-05 17:53:19 +0200

    feat: add badge to cart button in Header

--- X-course-task/src/components/Header/Header.tsx
+++ X-course-task/src/components/Header/Header.tsx
@@ -1,4 +1,6 @@
+import { useMemo } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useBooksContext } from '../../contexts';
 import { Button } from '../';
 
 import cartIcon from '../../assets/icons/cart.svg';
@@ -7,7 +9,10 @@ import avatarImage from '../../assets/images/avatar.png';
 import styles from './Header.module.scss';
 
 function Header() {
+  const { cartList } = useBooksContext();
   const navigate = useNavigate();
+
+  const allCount = useMemo(() => cartList.reduce((acc, item) => acc + item.count, 0), [cartList]);
 
   return (
     <header className={styles['header']}>
@@ -18,6 +23,7 @@ function Header() {
         <div className={styles['control']}>
           <button className={styles['button-cart']} onClick={() => navigate('/cart')}>
             <img src={cartIcon} alt="cart" />
+            {allCount > 0 && <span className={styles['badge']}>{allCount}</span>}
           </button>
           <Button onClick={() => console.log('Sign out')} typeStyleBtn="transparent">
             Sign out

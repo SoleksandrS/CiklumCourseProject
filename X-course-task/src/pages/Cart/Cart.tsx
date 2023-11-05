7708ca77ba3263d55181b964e5c13e683740ba78
SoleksandrS <o.y.sitaylo@student.khai.edu>
2023-11-05 14:01:44 +0200

    feat: add Button and empty message for Cart page

--- X-course-task/src/pages/Cart/Cart.tsx
+++ X-course-task/src/pages/Cart/Cart.tsx
@@ -1,7 +1,28 @@
+import { useMemo } from 'react';
+import { Button } from '../../components';
+
+import cartIcon from '../../assets/icons/cart.svg';
+
+import styles from './Cart.module.scss';
+
 function Cart() {
+  const cartList = useMemo(() => [], []);
+
   return (
-    <div>
-      <h1>Cart</h1>
+    <div className={styles['cart']}>
+      <div className={styles['top-line']}>
+        <Button>Purchase</Button>
+      </div>
+      <div className={styles['main-content']}>
+        {cartList.length > 0 ? (
+          <div>List</div>
+        ) : (
+          <div className={styles['empty']}>
+            <img className={styles['empty-image']} src={cartIcon} alt="Cart" />
+            <p className={styles['empty-text']}>Cart empty ..</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
